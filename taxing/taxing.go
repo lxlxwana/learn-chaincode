07e998b63cc5b24d5b0e962189c5763c4aa1bec1
xiao.liang <lxlxwana@gmail.com>
2017-03-09 14:34:39 +0800

    new

--- taxing/taxing.go
+++ taxing/taxing.go
@@ -5,6 +5,8 @@ import (
 
 	"errors"
 
+	"strconv"
+
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 )
 
@@ -34,9 +36,23 @@ func main() {
 
 // Init is
 func (c *Chaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-	err := stub.CreateTable("personfile", []*shim.ColumnDefinition{
-		&shim.ColumnDefinition{Name: "name", Type: shim.ColumnDefinition_STRING, Key: true},
-		&shim.ColumnDefinition{Name: "age", Type: shim.ColumnDefinition_INT32, Key: false},
+	err := stub.CreateTable("orders", []*shim.ColumnDefinition{
+		&shim.ColumnDefinition{Name: "id", Type: shim.ColumnDefinition_UINT64, Key: true},
+		&shim.ColumnDefinition{Name: "passenger", Type: shim.ColumnDefinition_STRING, Key: true},
+		&shim.ColumnDefinition{Name: "driver", Type: shim.ColumnDefinition_STRING, Key: true},
+		&shim.ColumnDefinition{Name: "startX", Type: shim.ColumnDefinition_INT64, Key: false},
+		&shim.ColumnDefinition{Name: "startY", Type: shim.ColumnDefinition_INT64, Key: false},
+		&shim.ColumnDefinition{Name: "destinationX", Type: shim.ColumnDefinition_INT64, Key: false},
+		&shim.ColumnDefinition{Name: "destinationY", Type: shim.ColumnDefinition_INT64, Key: false},
+		&shim.ColumnDefinition{Name: "preFee", Type: shim.ColumnDefinition_INT32, Key: false},
+		&shim.ColumnDefinition{Name: "actFeeTime", Type: shim.ColumnDefinition_INT32, Key: false},
+		&shim.ColumnDefinition{Name: "actFeeDis", Type: shim.ColumnDefinition_INT32, Key: false},
+		&shim.ColumnDefinition{Name: "startTime", Type: shim.ColumnDefinition_UINT64, Key: true},
+		&shim.ColumnDefinition{Name: "pickTime", Type: shim.ColumnDefinition_UINT64, Key: false},
+		&shim.ColumnDefinition{Name: "endTime", Type: shim.ColumnDefinition_UINT64, Key: false},
+		&shim.ColumnDefinition{Name: "state", Type: shim.ColumnDefinition_INT32, Key: true},
+		&shim.ColumnDefinition{Name: "passInfo", Type: shim.ColumnDefinition_STRING, Key: false},
+		&shim.ColumnDefinition{Name: "driverInfo", Type: shim.ColumnDefinition_STRING, Key: false},
 	})
 	if err != nil {
 		return nil, err
@@ -57,8 +73,8 @@ func (c *Chaincode) Invoke(stub shim.ChaincodeStubInterface, function string, ar
 		return c.write(stub, args)
 	case "ping":
 		return c.ping(stub)
-	case "settable":
-		return c.setTable(stub, args)
+	case "setOrder":
+		return c.setOrder(stub, args)
 	}
 
 	fmt.Println("Invoke did not find func: " + function)
@@ -75,8 +91,8 @@ func (c *Chaincode) Query(stub shim.ChaincodeStubInterface, function string, arg
 		return c.read(stub, args)
 	case "ping":
 		return c.ping(stub)
-	case "gettable":
-		return c.getTable(stub, args)
+	case "getOrder":
+		return c.getOrder(stub, args)
 	}
 
 	fmt.Println("Query did not find func: " + function)
@@ -87,50 +103,126 @@ func (c *Chaincode) ping(stub shim.ChaincodeStubInterface) ([]byte, error) {
 	return []byte("Hello, world!"), nil
 }
 
-func (c *Chaincode) setTable(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
-	// col1Val := args[0]
-	// col2Int, err := strconv.ParseInt(args[1], 10, 32)
-	// if err != nil {
-	// 	return nil, errors.New("insertTableOne failed. arg[1] must be convertable to int32")
-	// }
-	// col2Val := int32(col2Int)
-	// col3Int, err := strconv.ParseInt(args[2], 10, 32)
-	// if err != nil {
-	// 	return nil, errors.New("insertTableOne failed. arg[2] must be convertable to int32")
-	// }
-	// col3Val := int32(col3Int)
+func (c *Chaincode) setOrder(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	if len(args) != 16 {
+		return nil, errors.New("Expecting 16 arguments")
+	}
+
+	var id, startTime, pickTime, endTime uint64
+	var passenger, driver, passInfo, driverInfo string
+	var startX, startY, destinationX, destinationY, temp int64
+	var preFee, actFeeTime, actFeeDis, state int32
+
+	id, err := strconv.ParseUint(args[0], 10, 64)
+	if err != nil {
+		return nil, errors.New("args[0] must be convertable to uint64")
+	}
+	passenger = args[1]
+	driver = args[2]
+	startX, err = strconv.ParseInt(args[3], 10, 64)
+	if err != nil {
+		return nil, errors.New("args[3] must be convertable to int64")
+	}
+	startY, err = strconv.ParseInt(args[4], 10, 64)
+	if err != nil {
+		return nil, errors.New("args[4] must be convertable to int64")
+	}
+	destinationX, err = strconv.ParseInt(args[5], 10, 64)
+	if err != nil {
+		return nil, errors.New("args[5] must be convertable to int64")
+	}
+	destinationY, err = strconv.ParseInt(args[6], 10, 64)
+	if err != nil {
+		return nil, errors.New("args[6] must be convertable to int64")
+	}
+	temp, err = strconv.ParseInt(args[7], 10, 32)
+	if err != nil {
+		return nil, errors.New("args[7] must be convertable to int32")
+	}
+	preFee = int32(temp)
+	temp, err = strconv.ParseInt(args[8], 10, 32)
+	if err != nil {
+		return nil, errors.New("args[8] must be convertable to int32")
+	}
+	actFeeTime = int32(temp)
+	temp, err = strconv.ParseInt(args[9], 10, 32)
+	if err != nil {
+		return nil, errors.New("args[9] must be convertable to int32")
+	}
+	actFeeDis = int32(temp)
+	startTime, err = strconv.ParseUint(args[10], 10, 64)
+	if err != nil {
+		return nil, errors.New("args[10] must be convertable to uint64")
+	}
+	pickTime, err = strconv.ParseUint(args[11], 10, 64)
+	if err != nil {
+		return nil, errors.New("args[11] must be convertable to uint64")
+	}
+	endTime, err = strconv.ParseUint(args[12], 10, 64)
+	if err != nil {
+		return nil, errors.New("args[12] must be convertable to uint64")
+	}
+	temp, err = strconv.ParseInt(args[13], 10, 32)
+	if err != nil {
+		return nil, errors.New("args[13] must be convertable to int32")
+	}
+	state = int32(temp)
+	passInfo = args[14]
+	driverInfo = args[15]
 
 	var columns []*shim.Column
-	col1 := shim.Column{Value: &shim.Column_String_{String_: "liang"}}
-	col2 := shim.Column{Value: &shim.Column_Int32{Int32: 18}}
-	columns = append(columns, &col1)
-	columns = append(columns, &col2)
+	col1 := shim.Column{Value: &shim.Column_Uint64{Uint64: id}}
+	col2 := shim.Column{Value: &shim.Column_String_{String_: passenger}}
+	col3 := shim.Column{Value: &shim.Column_String_{String_: driver}}
+	col4 := shim.Column{Value: &shim.Column_Int64{Int64: startX}}
+	col5 := shim.Column{Value: &shim.Column_Int64{Int64: startY}}
+	col6 := shim.Column{Value: &shim.Column_Int64{Int64: destinationX}}
+	col7 := shim.Column{Value: &shim.Column_Int64{Int64: destinationY}}
+	col8 := shim.Column{Value: &shim.Column_Int32{Int32: preFee}}
+	col9 := shim.Column{Value: &shim.Column_Int32{Int32: actFeeTime}}
+	col10 := shim.Column{Value: &shim.Column_Int32{Int32: actFeeDis}}
+	col11 := shim.Column{Value: &shim.Column_Uint64{Uint64: startTime}}
+	col12 := shim.Column{Value: &shim.Column_Uint64{Uint64: pickTime}}
+	col13 := shim.Column{Value: &shim.Column_Uint64{Uint64: endTime}}
+	col14 := shim.Column{Value: &shim.Column_Int32{Int32: state}}
+	col15 := shim.Column{Value: &shim.Column_String_{String_: passInfo}}
+	col16 := shim.Column{Value: &shim.Column_String_{String_: driverInfo}}
+
+	columns = append(columns, &col1, &col2, &col3, &col4, &col5, &col6, &col7, &col8)
+	columns = append(columns, &col9, &col10, &col11, &col12, &col13, &col14, &col15, &col16)
 	row := shim.Row{Columns: columns}
-	ok, err := stub.InsertRow("personfile", row)
+
+	ok, err := stub.InsertRow("orders", row)
 	if err != nil {
 		return nil, fmt.Errorf("insert Table operation failed. %s", err)
 	}
 	if !ok {
-		return nil, errors.New("insert Table operation failed. Row with given key already exists")
+		ok, err := stub.ReplaceRow("orders", row)
+		if err != nil {
+			return nil, fmt.Errorf("replace Row operation failed. %s", err)
+		}
+		if !ok {
+			return nil, errors.New("replace Row operation failed. Row with given key does not exist")
+		}
 	}
 
 	return nil, err
 }
 
-func (c *Chaincode) getTable(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+func (c *Chaincode) getOrder(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	if len(args) < 1 {
+		return nil, errors.New("getOrder failed. Must include at least 1 key value")
+	}
 
-	// 	if len(args) < 1 {
-	// 	return nil, errors.New("getRowTableOne failed. Must include 1 key value")
-	// }
-
+	var id uint64
+	id, err := strconv.ParseUint(args[0], 10, 64)
 	var columns []shim.Column
-	col1 := shim.Column{Value: &shim.Column_String_{String_: "liang"}}
+	col1 := shim.Column{Value: &shim.Column_Uint64{Uint64: id}}
 	columns = append(columns, col1)
-	row, err := stub.GetRow("personfile", columns)
+	row, err := stub.GetRow("orders", columns)
 	if err != nil {
 		return nil, fmt.Errorf("getRow Table operation failed. %s", err)
 	}
-
 	rowString := fmt.Sprintf("%s", row)
 	return []byte(rowString), nil
 }
